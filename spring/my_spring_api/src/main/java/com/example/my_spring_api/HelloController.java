ba3a1ee52d24cbbe36c99edcd9b55b1533babf5f
kortyeltefari <kortyel@tefari.com>
2022-06-14 21:35:53 -0500

    develop

--- spring/my_spring_api/src/main/java/com/example/my_spring_api/HelloController.java
+++ spring/my_spring_api/src/main/java/com/example/my_spring_api/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.my_spring_api;
+
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(path = "/")
 public class HelloController {
+    @ModelAttribute
+    private void setResponseHeader(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+    }
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(path = "/")
+    public @ResponseBody
+    String hello() {
+        return "hello";
+    }
 
-        @GetMapping(path = "/")
-        public @ResponseBody 
-        String hello(){
-            return "hello";
-        }
-    
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(path = "/get")
+    public @ResponseBody
+    String get() {
+        String response ="{\"status\":\"example\"}";
+        return response;
+    }
+
 }
